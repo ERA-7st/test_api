aa9dd2b6bcf32a0ab792ce3aa1b18b62c90ba7d8
ERA-7st <are123230k@gmail.com>
2022-06-22 11:09:12 +0900

    add_ideas_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'ideas/index'
+  get 'ideas/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :ideas, only: [:index, :create]
